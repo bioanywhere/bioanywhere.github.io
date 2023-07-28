34923684c802006254fe9061ead2203620aaccb5
bioanywhere <agarcia@g-forward.com>
2023-07-28 11:10:38 -0500

    Fix urls and tokens

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import utils from './utils.js'
 
-let CLIENT_ID = "200624216191-au85bp336m9kj1vpfqajhogl0addg5he.apps.googleusercontent.com"
-let REDIRECT_URI = "https://bioanywhere.github.io/grist-widget/google/profile.html"
+let CLIENT_ID = "200624216191-nujs4nugdo4171450bfmm55rk5fksdej.apps.googleusercontent.com"
+let REDIRECT_URI = "https://bioanywhere.github.io/profile.html"
 let SCOPES = "https://www.googleapis.com/auth/userinfo.profile"
 
 let button = document.getElementById('button')
