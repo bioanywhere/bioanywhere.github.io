9ba1836a19fac30002aade03737165e7dc3c2bc3
bioanywhere <agarcia@g-forward.com>
2023-07-28 17:55:42 -0500

    try again
    try again

--- profile.js
+++ profile.js
@@ -18,80 +18,41 @@ fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
     console.log("User Info:", info);
     document.getElementById("name").innerHTML += info.name;
     document.getElementById("image").setAttribute("src", info.picture);
-
-    // Proceed with the "Create Report" button event handling
-    let button = document.getElementById("Report");
-    button.onclick = async () => {
-      // Retrieve the access_token and json_data from the local storage
-      const access_token = JSON.parse(localStorage.getItem("info")).access_token;
-      const json_data = JSON.parse(localStorage.getItem("json_data"));
-
-      console.log("Access Token:", access_token);
-      console.log("JSON Data:", json_data);
-
-      try {
-        // Call the function to upload the document to Google Drive
-        const documentUrl = await uploadToGoogleDrive(access_token, json_data);
-
-        // Step 4: Redirect the user to the new document after clicking on the "Create Report" button
-        if (documentUrl) {
-          console.log("Step 4: Redirecting to the new document:", documentUrl);
-          window.location.href = documentUrl;
-        } else {
-          alert('Failed to create the report. Please try again later.');
-        }
-      } catch (error) {
-        console.error('Error creating the report:', error);
-        alert('Failed to create the report. Please try again later.');
-      }
-    };
   })
   .catch((error) => {
     console.error("Error fetching user info:", error);
   });
 
-// Function to upload the document to Google Drive using Google Drive API
-async function uploadToGoogleDrive(access_token, json_data) {
+// Event listener for the "Create Report" button
+document.getElementById('Report').addEventListener('click', async () => {
+  // Retrieve the access_token and json_data from the local storage
+  const access_token = JSON.parse(localStorage.getItem("info")).access_token;
+  const json_data = JSON.parse(localStorage.getItem("json_data"));
+
+  console.log("Access Token:", access_token);
+  console.log("JSON Data:", json_data);
+
   try {
-    const createFileResponse = await fetch('https://www.googleapis.com/drive/v3/files', {
+    // Step 1: Create a new document in Google Docs with JSON content
+    console.log("Step 1: Creating a new document in Google Docs...");
+    const createFileResponse = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=media', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${access_token}`,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        name: 'New Document', // Set the name of the new document
-        mimeType: 'application/vnd.google-apps.document'
-      })
+      body: JSON.stringify(json_data),
     });
 
     const responseData = await createFileResponse.json();
     console.log("Step 1: Response data:", responseData);
 
-    if (responseData.id) {
-      const documentId = responseData.id;
-      console.log("Step 2: Inserting json_data in the new document...");
-      const importResponse = await fetch(`https://www.googleapis.com/upload/drive/v3/files/${documentId}/import`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(json_data)
-      });
-
-      const importResponseData = await importResponse.json();
-      console.log("Step 2: Import Response data:", importResponseData);
-
-      // Step 3: Return the URL of the new document just created
-      const documentUrl = `https://docs.google.com/document/d/${documentId}`;
-      console.log("Step 3: Document URL:", documentUrl);
-      return documentUrl;
-    } else {
-      throw new Error('Failed to create a new document.');
-    }
+    // Step 2: Return the URL of the new document just created
+    const documentUrl = `https://docs.google.com/document/d/${responseData.id}`;
+    console.log("Step 2: Document URL:", documentUrl);
+    window.location.href = documentUrl;
   } catch (error) {
     console.error('Error creating the report:', error);
-    throw error;
+    alert('Failed to create the report. Please try again later.');
   }
-}
+});
