f5e2f88c0d35a2777aff3ea7a81441857cab7cf6
bioanywhere <agarcia@g-forward.com>
2023-07-28 21:51:05 -0500

    Big update.

--- profile.js
+++ profile.js
@@ -1,4 +1,3 @@
-
 import utils from "./utils.js";
 
 // Function to create the multipart request body with metadata and JSON content
@@ -62,22 +61,81 @@ document.getElementById('Report').addEventListener('click', async () => {
   console.log("JSON Data:", json_data);
 
   try {
-    // Step 1: Create a new document in Google Docs with JSON content and metadata
-    console.log("Step 1: Creating a new document in Google Docs...");
-    const createFileResponse = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
+    // Step 1: Duplicate the template document
+    console.log("Step 1: Duplicating the template document...");
+    const templateDocumentId = 'YOUR_TEMPLATE_DOCUMENT_ID'; // Replace this with your actual Google Docs template document ID
+    const duplicateResponse = await fetch(`https://www.googleapis.com/drive/v3/files/${templateDocumentId}/copy`, {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/json',
       },
-      body: utils.createMultipartRequestBody(json_data), // Call the function to create the multipart request body
+      body: JSON.stringify({
+        name: "My Report", // The name of the duplicated file
+      }),
     });
 
-    const responseData = await createFileResponse.json();
-    console.log("Step 1: Response data:", responseData);
+    const duplicateData = await duplicateResponse.json();
+    console.log("Step 1: Duplicated document ID:", duplicateData.id);
 
-    // Step 2: Return the URL of the new document just created
-    const documentUrl = `https://docs.google.com/document/d/${responseData.id}`;
-    console.log("Step 2: Document URL:", documentUrl);
+    // Step 2: Retrieve the content of the duplicated document
+    console.log("Step 2: Retrieving the content of the duplicated document...");
+    const contentResponse = await fetch(`https://www.googleapis.com/drive/v3/files/${duplicateData.id}/export?mimeType=application/vnd.openxmlformats-officedocument.wordprocessingml.document`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const contentBlob = await contentResponse.blob();
+    console.log("Step 2: Retrieved document content.");
+
+    // Step 3: Read the content of the duplicated document as text
+    console.log("Step 3: Reading the content of the duplicated document as text...");
+    const contentText = await contentBlob.text();
+
+ // Step 4: Replace the variables in the content with JSON data
+    console.log("Step 4: Replacing variables in the document content...");
+
+    // Function to replace variables in the content using the JSON data
+    function replaceVariables(match, variableName) {
+      // Traverse the JSON data dynamically using the variable name
+      const keys = variableName.split('.');
+      let value = json_data;
+      for (const key of keys) {
+        value = value[key];
+      }
+      // Return the value to be replaced in the template
+      return value;
+    }
+
+    // Use a regular expression to find and replace all variables in the content
+    const replacedContent = contentText.replace(/\{\{(.+?)\}\}/g, replaceVariables);
+
+
+  } catch (error) {
+    console.error('Error creating the report:', error);
+    alert('Failed to create the report. Please try again later.');
+  }
+});
+
+
+    // Step 5: Upload the modified content back to the document
+    console.log("Step 5: Uploading the modified content to the document...");
+    const updatedContentBlob = new Blob([replacedContent], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+    const updateContentResponse = await fetch(`https://www.googleapis.com/upload/drive/v3/files/${duplicateData.id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      },
+      body: updatedContentBlob,
+    });
+
+    console.log("Step 5: Document content updated successfully.");
+
+    // Step 6: Return the URL of the modified document
+    const documentUrl = `https://docs.google.com/document/d/${duplicateData.id}`;
+    console.log("Step 6: Document URL:", documentUrl);
     window.location.href = documentUrl;
   } catch (error) {
     console.error('Error creating the report:', error);
