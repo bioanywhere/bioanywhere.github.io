8c1d8a3f3f0d8ff0ba25f346d716979a9d43d910
bioanywhere <agarcia@g-forward.com>
2023-07-29 00:12:16 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -104,16 +104,57 @@ document.getElementById('Report').addEventListener('click', async () => {
 // Step 4: Replace the variables in the content with JSON data
 console.log("Step 4: Replacing variables in the document content...");
 
-// Function to replace variables in the content using the provided JSON data
+// Function to manually replace variables in the content using the provided JSON data
 function replaceVariables(match, variableName) {
-  const value = getNestedProperty(json_data, variableName);
+  // Manually map the variableName to the corresponding value in the json_data object
+  const mapping = {
+    "Company.Name": json_data.Company.Name,
+    "Company.Date_Founded": json_data.Company.Date_Founded,
+    "Company.Address": json_data.Company.Address,
+    "Company.Phone_Number": json_data.Company.Phone_Number,
+    "Company.Website": json_data.Company.Website,
+    "Company.Sector": json_data.Company.Sector,
+    "Company.Description": json_data.Company.Description,
+    "Company.Gross_Profit": json_data.Company.Gross_Profit,
+    "Company.Revenue": json_data.Company.Revenue,
+    "Company.Gross_Profit_Margin": json_data.Company.Gross_Profit_Margin,
+    "Company.Full_Time_Employees": json_data.Company.Full_Time_Employees,
+    "Company.Part_Time_Employees": json_data.Company.Part_Time_Employees,
+    "Company.Part_Time_Employee_Composition": json_data.Company.Part_Time_Employee_Composition,
+    "Company.Number_of_Owners": json_data.Company.Number_of_Owners,
+    "Company.Women_Owners": json_data.Company.Women_Owners,
+    "Company.Women_Owners_Ratio": json_data.Company.Women_Owners_Ratio,
+    "Company.Indigenous_Owners": json_data.Company.Indigenous_Owners,
+    "Company.Indigenous_Owners_Ratio": json_data.Company.Indigenous_Owners_Ratio,
+    "Company.Contact_Information": json_data.Company.Contact_Information,
+    "Company.Full_Time_Employee_Composition": json_data.Company.Full_Time_Employee_Composition,
+    "Company.Governance[0].Purpose": json_data.Company.Governance[0].Purpose,
+    "Company.Governance[0].Executive_Responsibilities": json_data.Company.Governance[0].Executive_Responsibilities,
+    "Company.Governance[0].Stakeholder_Engagement": json_data.Company.Governance[0].Stakeholder_Engagement,
+    "Company.Governance[0].Strategic_Planning": json_data.Company.Governance[0].Strategic_Planning,
+    "Company.Governance[0].Risk_Management_Process": json_data.Company.Governance[0].Risk_Management_Process,
+    "Company.Governance[0].Disclosures_Reports": json_data.Company.Governance[0].Disclosures_Reports,
+    "Company.Governance[0].Decision_Making": json_data.Company.Governance[0].Decision_Making,
+    "Company.Governance[0].Research_and_Development": json_data.Company.Governance[0].Research_and_Development,
+    "Company.Governance[0].Competency_Maps": json_data.Company.Governance[0].Competency_Maps,
+    "Company.Governance[0].Executive_Compensation": json_data.Company.Governance[0].Executive_Compensation,
+    "Company.Governance[0].Description": json_data.Company.Governance[0].Description,
+    "Company.Governance[0].Overall_Score": json_data.Company.Governance[0].Overall_Score,
+    "Company.Business_Model[0].Traditional_Flows": json_data.Company.Business_Model[0].Traditional_Flows,
+    "Company.Business_Model[0].Nested_Interdependencies": json_data.Company.Business_Model[0].Nested_Interdependencies,
+    "Company.Business_Model[0].Impact_Points": json_data.Company.Business_Model[0].Impact_Points,
+    "Company.Business_Model[0].Value": json_data.Company.Business_Model[0].Value,
+    "Company.Business_Model[0].Risks": json_data.Company.Business_Model[0].Risks,
+    "Company.Business_Model[0].Reputation": json_data.Company.Business_Model[0].Reputation,
+    "Company.Business_Model[0].Score": json_data.Company.Business_Model[0].Score,
+  };
 
-  // Handle special case where value is an object, array, or null
-  if (typeof value === "object" || value === null) {
-    return JSON.stringify(value);
+  // Check if the variableName exists in the mapping
+  if (mapping.hasOwnProperty(variableName)) {
+    return mapping[variableName];
+  } else {
+    return `{{${variableName}}}`; // Fallback to the original placeholder if key not found
   }
-
-  return value !== undefined ? value : `{{${variableName}}}`; // Fallback to the original placeholder if key not found
 }
 
 // Use a regular expression to find and replace all variables in the content
@@ -139,11 +180,8 @@ console.log("Step 5: Document content updated successfully.");
 const documentUrl = `https://docs.google.com/document/d/${duplicateData.id}`;
 console.log("Step 6: Document URL:", documentUrl);
 window.location.href = documentUrl;
-
- catch (error) {
-  console.error('Error updating the document content:', error);
-  alert('Failed to update the document content. Please try again later.');
-}
-
+  } catch (error) {
+    console.error('Error creating the report:', error);
+    alert('Failed to create the report. Please try again later.');
   }
 });
