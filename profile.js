6b0685edcbfcf7cd8a2ce6d1f8e4a78120b3641b
bioanywhere <agarcia@g-forward.com>
2023-07-29 22:26:37 -0500

    put

--- profile.js
+++ profile.js
@@ -1,3 +1,70 @@
+import utils from "./utils.js";
+
+// Function to get nested properties from an object based on a dot-separated string
+function getNestedProperty(obj, propString) {
+  const props = propString.split('.');
+  let value = obj;
+  for (const prop of props) {
+    if (value.hasOwnProperty(prop)) {
+      value = value[prop];
+    } else {
+      return undefined; // Return undefined if the property doesn't exist
+    }
+  }
+  return value;
+}
+
+// Function to create the multipart request body with metadata and JSON content
+utils.createMultipartRequestBody = (json_data) => {
+  // Define the metadata for the file (change as needed)
+  const metadata = {
+    name: "My Report", // The name of the file
+    mimeType: "application/vnd.google-apps.document", // MIME type for Google Docs document
+  };
+
+  // Create the multipart request body
+  const boundary = "-------" + Date.now();
+  const delimiter = `\r\n--${boundary}\r\n`;
+  const closeDelimiter = `\r\n--${boundary}--`;
+
+  // Construct the metadata part of the request
+  let requestBody = delimiter;
+  requestBody += `Content-Type: application/json; charset=UTF-8\r\n\r\n`;
+  requestBody += JSON.stringify(metadata);
+
+  // Add the JSON content part of the request
+  requestBody += delimiter;
+  requestBody += `Content-Type: application/json\r\n\r\n`;
+  requestBody += JSON.stringify(json_data);
+
+  // Add the closing boundary
+  requestBody += closeDelimiter;
+
+  return requestBody;
+};
+
+let params = utils.getParamsFromURL(location.href);
+let redirect_url = "";
+
+console.log("params:", params);
+
+utils.saveOAuth2Info(params, "profile.html", "info");
+
+fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+  headers: {
+    Authorization: `Bearer ${params.access_token}`,
+  },
+})
+  .then((data) => data.json())
+  .then((info) => {
+    console.log("User Info:", info);
+    document.getElementById("name").innerHTML += info.name;
+    document.getElementById("image").setAttribute("src", info.picture);
+  })
+  .catch((error) => {
+    console.error("Error fetching user info:", error);
+  });
+
 // ... (previous code)
 
 // Event listener for the "Create Report" button
