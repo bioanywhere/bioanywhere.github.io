be99e4a485dd9e1ed192dd9492c7ad0322002f6c
bioanywhere <agarcia@g-forward.com>
2023-07-30 01:52:00 -0500

    testing.

--- profile.js
+++ profile.js
@@ -43,7 +43,32 @@ utils.createMultipartRequestBody = (json_data) => {
   return requestBody;
 };
 
+let params = utils.getParamsFromURL(location.href);
+let redirect_url = "";
 
+console.log("params:", params);
+
+utils.saveOAuth2Info(params, "profile.html", "info");
+
+fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+  headers: {
+    Authorization: `Bearer ${params.access_token}`,
+  },
+})
+  .then((data) => data.json())
+  .then((info) => {
+    console.log("User Info:", info);
+    document.getElementById("name").innerHTML += info.name;
+    document.getElementById("image").setAttribute("src", info.picture);
+  })
+  .catch((error) => {
+    console.error("Error fetching user info:", error);
+  });
+
+
+// ... (Rest of the code)
+
+// Function to convert JSON data to a DataFrame-like array of objects
 function flattenJson(data, prefix = '', result = []) {
   for (const key in data) {
     if (data.hasOwnProperty(key)) {
@@ -75,84 +100,108 @@ function convertJsonToDataFrame(jsonData) {
   return reportData;
 }
 
-// Example usage with the provided JSON data
-const jsonData = '{"id":2,"Company":"Forward","Report":"{' + "'id': 2, 'Company': {'id': 1, 'Name': 'Forward', 'Date_Founded': 2007.0, 'Address': '', 'Phone_Number': 3008156298.0, 'Website': 'www.g-forward.com', 'Sector': null, 'Description': 'Software Development', 'Gross_Profit': 20.0, 'Revenue': 70.0, 'Gross_Profit_Margin': 28.57142857142857, 'Full_Time_Employees': 80.0, 'Part_Time_Employees': 20.0, 'Part_Time_Employee_Composition': 0.2, 'Number_of_Owners': 30.0, 'Women_Owners': 3.0, 'Women_Owners_Ratio': 0.1, 'Indigenous_Owners': 20.0, 'Indigenous_Owners_Ratio': 0.6666666666666666, 'Contact_Information': 'Pepe Mujica', 'Full_Time_Employee_Composition': 0.8, 'Governance': 'Governance[RecordList([2], group_by={' + "'Company': 1}, sort_by=None)], 'References': null, 'Business_Model': 'Business_Model[[1]]}, 'Governance': [{'id': 1, 'Purpose': 'No', 'Executive_Responsibilities': 'No', 'Stakeholder_Engagement': 'Partially', 'Strategic_Planning': 'Yes', 'Risk_Management_Process': 'Partially', 'Disclosures_Reports': 'Partially', 'Decision_Making': 'Yes', 'Research_and_Development': 'Partially', 'Competency_Maps': 'Yes', 'Executive_Compensation': 'No', 'Company': 'Companies[2]', 'Description': 'Consumer Insights', 'Sum_No': 3, 'Sum_Partially': 4, 'Sum_Yes': 3, 'Overall_Score': 0.5, 'Weight_No': 0, 'Weight_Yes': 10, 'Weight_Partially': 5}], 'Business_Model': [{'id': 2, 'Traditional_Flows': true, 'Nested_Interdependencies': false, 'Impact_Points': false, 'Value': false, 'Risks': true, 'Reputation': true, 'Company': 'Companies[2]', 'Score': 0.5}], 'Send_JSON': null, 'Transaction': null}","Governance":{"tableId":"Governance","rowIds":[1]},"Business_Model":{"tableId":"Business_Model","rowIds":[2]},"Send_JSON":null,"Transaction":null}';
-const dataFrame = convertJsonToDataFrame(jsonData);
-console.log(dataFrame);
-
-
-
-
-let params = utils.getParamsFromURL(location.href);
-let redirect_url = "";
-
-console.log("params:", params);
-
-utils.saveOAuth2Info(params, "profile.html", "info");
-
-fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
-  headers: {
-    Authorization: `Bearer ${params.access_token}`,
-  },
-})
-  .then((data) => data.json())
-  .then((info) => {
-    console.log("User Info:", info);
-    document.getElementById("name").innerHTML += info.name;
-    document.getElementById("image").setAttribute("src", info.picture);
-  })
-  .catch((error) => {
-    console.error("Error fetching user info:", error);
-  });
-
-// ... (Rest of the code)
-
 // Event listener for the "Create Report" button
 document.getElementById('Report').addEventListener('click', async () => {
-  // ... (Previous code)
+  console.log("Button clicked.");
+  // Retrieve the access_token from the local storage
+  const access_token = JSON.parse(localStorage.getItem("info")).access_token;
+  console.log("Access Token:", access_token);
+
+  // Retrieve the JSON data from the local storage
+  const jsonData = JSON.parse(localStorage.getItem("json_data"));
+  const dataFrame = convertJsonToDataFrame(jsonData);
+  console.log("DataFrame:", dataFrame);
+
+  // Check if json_data exists
+  if (!jsonData) {
+    console.error("JSON data not found in local storage.");
+  } else {
+    // Print the JSON data in the console
+    console.log("JSON data*:", jsonData);
+  }
+
+  // Function to store the template document ID in the local storage
+  function setTemplateDocumentId(templateDocumentId) {
+    localStorage.setItem("templateDocumentId", templateDocumentId);
+  }
+
+  const templateDocumentId = '132dW6-cb5w1io1tB8qkc6W1wpA2xpEntugZezycyUa0';
+  setTemplateDocumentId(templateDocumentId);
+  console.log("Template Document ID:", templateDocumentId);
 
   try {
     // Step 1: Duplicate the template document
-    // ... (Previous code)
+    console.log("Step 1: Duplicating the template document...");
+    const duplicateResponse = await fetch(`https://www.googleapis.com/drive/v3/files/${templateDocumentId}/copy`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: "My Report", // The name of the duplicated file
+      }),
+    });
 
-    // Step 2: Convert JSON to DataFrame-like array of objects
-    console.log("Step 2: Converting JSON to DataFrame-like array of objects...");
-    const jsonData = '{"id":2,"Company":"Forward","Report":"{' + "'id': 2, 'Company': {'id': 1, 'Name': 'Forward', ..."; // Your JSON data here
-    const dataFrame = convertJsonToDataFrame(jsonData);
-    console.log("DataFrame:", dataFrame);
+    const duplicateData = await duplicateResponse.json();
+    console.log("Step 1: Duplicated document ID:", duplicateData.id);
 
-    // Step 3: Retrieve the content of the duplicated document
-    console.log("Step 3: Retrieving the content of the duplicated document...");
-    const contentResponse = await fetch(`https://www.googleapis.com/drive/v3/files/${duplicateData.id}?alt=media`, {
+    console.log("Step 2: Document content duplicated successfully.");
+
+    // Step 3: Replace placeholders in the duplicated document with corresponding values from DataFrame
+    console.log("Step 3: Replacing placeholders in the document...");
+
+    // Retrieve the content of the duplicated document
+    const contentResponse = await fetch(`https://www.googleapis.com/drive/v3/files/${duplicateData.id}/export?mimeType=application/vnd.openxmlformats-officedocument.wordprocessingml.document`, {
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
     });
 
-    const contentText = await contentResponse.text();
+    const contentBlob = await contentResponse.blob();
     console.log("Step 3: Retrieved document content.");
 
-    // Step 4: Replace Placeholders with Values from DataFrame
-    console.log("Step 4: Replacing Placeholders with Values...");
-    let updatedContent = contentText;
-    for (const row of dataFrame) {
-      updatedContent = updatedContent.replace(new RegExp(row.Placeholder, 'g'), row.Value);
-    }
-    console.log("Step 4: Placeholders replaced with Values.");
+    // Read the content of the duplicated document as text
+    const contentText = await contentBlob.text();
 
-    // Step 5: Upload the modified content back to the document
-    console.log("Step 5: Uploading the modified content to the document...");
-    const updatedContentBlob = new Blob([updatedContent], { type: 'application/vnd.google-apps.document' });
+    // Replace placeholders with corresponding values from DataFrame
+    let finalContent = contentText;
+    dataFrame.forEach((data) => {
+      finalContent = finalContent.replace(new RegExp(data.Placeholder, 'g'), data.Value);
+    });
+    console.log("Step 3: Placeholders replaced with values.");
+
+    // Step 4: Upload the modified content back to the document
+    console.log("Step 4: Uploading the modified content to the document...");
+    const updatedContentBlob = new Blob([finalContent], { type: 'application/vnd.google-apps.document' });
 
     try {
       // Send the PATCH request to update the file content
-      // ... (Previous code for sending PATCH request)
+      console.log("Sending PATCH request to update file content...");
 
-      console.log("Step 5: Document content updated successfully.");
-      debugger;
-      // Step 6: Return the URL of the modified document
+      const updateContentUrl = `https://www.googleapis.com/upload/drive/v3/files/${duplicateData.id}`;
+      const updateContentHeaders = {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/vnd.google-apps.document',
+      };
+
+      const updateContentResponse = await fetch(updateContentUrl, {
+        method: 'PATCH',
+        headers: updateContentHeaders,
+        body: updatedContentBlob,
+      });
+
+      console.log("PATCH Request Sent.");
+
+      // Read the response as JSON
+      const updateContentResponseData = await updateContentResponse.clone().json();
+
+      console.log("PATCH Response Data:", updateContentResponseData);
+
+      console.log("Step 4: Document content updated successfully.");
+      // Step 5: Return the URL of the modified document
       const documentUrl = `https://docs.google.com/document/d/${duplicateData.id}`;
-      console.log("Step 6: Document URL:", documentUrl);
+      console.log("Step 5: Document URL:", documentUrl);
       debugger;
       window.location.href = documentUrl;
     } catch (error) {
