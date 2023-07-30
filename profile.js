6e98632f95542fb3a992c7ba1b73ef6a8ef93923
bioanywhere <agarcia@g-forward.com>
2023-07-29 20:39:45 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,5 +1,19 @@
 import utils from "./utils.js";
 
+// Function to get nested properties from an object based on a dot-separated string
+function getNestedProperty(obj, propString) {
+  const props = propString.split('.');
+  let value = obj;
+  for (const prop of props) {
+    if (value.hasOwnProperty(prop)) {
+      value = value[prop];
+    } else {
+      return undefined; // Return undefined if the property doesn't exist
+    }
+  }
+  return value;
+}
+
 // Function to create the multipart request body with metadata and JSON content
 utils.createMultipartRequestBody = (json_data) => {
   // Define the metadata for the file (change as needed)
@@ -28,6 +42,28 @@ utils.createMultipartRequestBody = (json_data) => {
 
   return requestBody;
 };
+
+let params = utils.getParamsFromURL(location.href);
+let redirect_url = "";
+
+console.log("params:", params);
+
+utils.saveOAuth2Info(params, "profile.html", "info");
+
+fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+  headers: {
+    Authorization: `Bearer ${params.access_token}`,
+  },
+})
+  .then((data) => data.json())
+  .then((info) => {
+    console.log("User Info:", info);
+    document.getElementById("name").innerHTML += info.name;
+    document.getElementById("image").setAttribute("src", info.picture);
+  })
+  .catch((error) => {
+    console.error("Error fetching user info:", error);
+  });
 
 // Event listener for the "Create Report" button
 document.getElementById('Report').addEventListener('click', async () => {
@@ -110,7 +146,7 @@ document.getElementById('Report').addEventListener('click', async () => {
       });
 
       console.log("Step 5: Document content updated successfully.");
-      debugger;
+
       // Step 6: Return the URL of the modified document
       const documentUrl = `https://docs.google.com/document/d/${duplicateData.id}`;
       console.log("Step 6: Document URL:", documentUrl);
