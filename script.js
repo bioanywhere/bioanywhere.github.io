89d94d33c876b815b6569e9b75d0f44b9208c1a9
bioanywhere <agarcia@g-forward.com>
2023-07-31 22:37:10 -0500

    logout

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ utils.signIn(CLIENT_ID,REDIRECT_URI,SCOPES)
 
 
 
-let button_logout = document.getElementById('logout') // Get the logout button from the profile.html file
+let logoutButton = document.getElementById('logout'); // Get the logout button from the profile.html file
 
-button_logout.addEventListener('click',logout)
+logoutButton.addEventListener('click', logoutAction);
 
-function logout(){
+function logoutAction() {
+  let oAuth2Data = JSON.parse(localStorage.getItem('info')); // Assuming the OAuth2 data is stored in localStorage with the key 'info'
+  let access_token = oAuth2Data.access_token;
+  let redirect_url = "https://bioanywhere.github.io/index.html"; // Replace with the URL to which you want to redirect after logout
 
-utils.logout(CLIENT_ID,REDIRECT_URI)
-
+  utils.logout(access_token, redirect_url);
 }
