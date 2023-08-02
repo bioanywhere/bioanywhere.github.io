18d4a92d9ad2d8fabcf3424032f76b75ced6221a
bioanywhere <agarcia@g-forward.com>
2023-08-01 20:17:48 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -152,32 +152,32 @@ function fetchUserInfo(params) {
 
 
 
-      // Replace the <name> placeholder with the user's full name
+      // Replace the <name> and <image> placeholders with actual values
       const nameElement = document.getElementById("name");
-      if (nameElement) {
-        nameElement.textContent = info.name;
-        // Show the name element after data is available
-        nameElement.style.display = "inline";
-      }
-
-      // Set the 'onload' event for the image element to ensure it's displayed after the image is fully loaded
       const imageElement = document.getElementById("image");
-      if (imageElement) {
+
+      // Check if the elements exist before updating the information
+      if (nameElement && imageElement) {
+        // Set the name element text to the user's name
+        nameElement.textContent = "Your Full Name is: " + info.name;
+
+        // Set the 'onload' event for the image element to ensure it's displayed after the image is fully loaded
         imageElement.onload = () => {
-          // Display the image element after it's loaded
+          // Show the image element after it's loaded
           imageElement.style.display = "inline";
         };
-        // Set the 'src' attribute to trigger the image load
+        // Set the image element source to the user's picture
         imageElement.src = info.picture;
+
+        // Show the name and image elements after data is available
+        nameElement.style.display = "inline";
+        imageElement.style.display = "inline";
       }
     })
     .catch((error) => {
       console.error("Error fetching user info:", error);
     });
 }
-
-
-
 
 
 // ... (Rest of the code)
