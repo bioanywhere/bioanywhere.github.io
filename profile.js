56daabddf7f2f37188d3189a2e48f8a3daac12de
bioanywhere <agarcia@g-forward.com>
2023-08-05 16:58:13 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -348,6 +348,7 @@ function callAnvilEndpoint(copiedSheetId, accessToken) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify(data),
   })
