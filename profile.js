e1d873f80a05dc071fbfdfe9fba721500b39bc49
bioanywhere <agarcia@g-forward.com>
2023-08-07 10:03:00 -0500

    Use copiedSheetId

--- profile.js
+++ profile.js
@@ -341,7 +341,7 @@ console.log("******Access Token:*****", accessToken);
 
 async function callWebAppWithAccessTokenAndSpreadsheetId(accessToken, copiedSheetId) {
   console.log("Calling Google Apps Script");
-  var webAppUrl = `https://script.google.com/macros/s/AKfycbwFjj2Gk0X3a8kiZW6krKUCR9ZxyKBFeu4dPIKONuZUQ7C8OCCR_XckH8jYLe7de9ia/exec?accessToken=${accessToken}&spreadsheetId=${spreadsheetId}`;
+  var webAppUrl = `https://script.google.com/macros/s/AKfycbwFjj2Gk0X3a8kiZW6krKUCR9ZxyKBFeu4dPIKONuZUQ7C8OCCR_XckH8jYLe7de9ia/exec?accessToken=${accessToken}&spreadsheetId=${copiedSheetId}`;
 
   var response = await fetch(webAppUrl);
   var data = await response.json();
