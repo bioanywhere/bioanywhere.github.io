7a3ca817c4d62b1bd49423d2e95b2ff99adedb7c
bioanywhere <agarcia@g-forward.com>
2023-08-10 12:02:43 -0500

    Update script.js
    Add https://www.googleapis.com/auth/script.projects in the scopes!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import utils from './utils.js'
 
 let CLIENT_ID = "200624216191-nujs4nugdo4171450bfmm55rk5fksdej.apps.googleusercontent.com"
 let REDIRECT_URI = "https://bioanywhere.github.io/profile.html"
-let SCOPES = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/script.external_request";
+let SCOPES = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/script.external_request https://www.googleapis.com/auth/script.projects";
 
 let button = document.getElementById('button')
 
