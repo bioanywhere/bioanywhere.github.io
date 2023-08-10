94514a37f437d9bc6ff743b6b3297ee801bf90a9
bioanywhere <agarcia@g-forward.com>
2023-08-10 14:24:43 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -316,7 +316,7 @@ console.log("Step 2: Calling Google Apps Script...");
 
 
 function callGoogleAppsScript() {
-  var scriptId = "AKfycbzrF4y_wkXUHzXCEE6nbTsNl9IJO5dfunA1bt0JFuhfdnJRZZMpbokLTUujUOVJmP9WEw"; // Replace with your actual script ID
+  var scriptId = "182sniLfIKzKnVM4ITiWYcrRZ4eLCjjRlZDDqFkg5fEoorHsDo6YHmdRW"; // Replace with your actual script ID
   var accessToken = access_token; // Replace with the access token
   var spreadsheetId = duplicateSheet.id; // Replace with the spreadsheet ID
 
